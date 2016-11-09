d4dd85cad318e2be86fa36bdfa69632ea77a63a4
jerrypy <jerrynwin@gmail.com>
2016-11-09 23:01:59 +0800

    init main

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+/*******************************************************
+ * main.cc -- Contains the main() function of Ven.     *
+ *                                                     *
+ *******************************************************
+ *                      *Ven*                          *
+ * Stands for VN, an LOL champion. And also stands for *
+ * V Engine, which is a salute to the famous and best  *
+ * web server in the world, Nginx(Engine X).           *
+ *                                                     *
+ *******************************************************
+ * Created by jerrypy on 11/09/2016 22:58              *
+ *                                                     *
+ *******************************************************/
+
+int main()
+{
+    return 0;
+}
